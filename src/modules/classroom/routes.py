7ba32344a58571d8a065933f6f2c4e1c4abf1d3b
Jaime Muñoz <jaime.munozq@udea.edu.co>
2025-05-05 21:59:21 -0500

    feat: :sparkles: Implement classroom management features: add models, routes, and upload functionality

--- src/modules/classroom/routes.py
+++ src/modules/classroom/routes.py
@@ -1,18 +1,67 @@
-
-
 from typing import List
-from fastapi import APIRouter
-from pydantic import BaseModel
-from src.modules.classroom.services import get_classrooms
-
-
-
+from fastapi import APIRouter, File, UploadFile
+from src.modules.classroom.models import Classroom, ClassroomRequest
+from src.modules.classroom.services import get_classrooms, create_classroom, update_classroom, delete_classroom
+from src.modules.classroom.services.upload_classrooms import upload_classrooms_from_excel
+from fastapi.responses import JSONResponse
+from fastapi.responses import Response
+from fastapi import HTTPException
 
 router = APIRouter(
     tags=["classroom"],
 )
 
-@router.get("/lists")
-async def create_group_list():
-    lists = await get_classrooms()
-    return {"classrooms available": lists}
+@router.get("/list", response_model=List[Classroom])
+async def get_classroom_list():
+    """
+    Get a list of classrooms.
+    """
+    try:
+        classrooms = await get_classrooms()
+        return JSONResponse(content=classrooms, status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/create", response_model=Classroom, status_code=201)
+async def create_classroomReClassroomRequest_endpoint(classroom_request: ClassroomRequest):
+    """
+    Create a new classroom.
+    """
+    try:
+        return await create_classroom(classroom_request)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/upload", status_code=201)
+async def upload_classrooms(file: UploadFile = File(...)):
+    """
+    Upload classrooms from an Excel file.
+    """
+    try:
+        await upload_classrooms_from_excel(file.file)
+        return JSONResponse(content={"message": "Classrooms uploaded successfully"}, status_code=201)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.put("/update/{classroom_id}", response_model=Classroom, status_code=200)
+async def update_classroom_endpoint(classroom_id: int, classroom_request: ClassroomRequest):
+    """
+    Update an existing classroom.
+    """
+    try:
+        return await update_classroom(classroom_id, classroom_request)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.delete("/delete/{classroom_id}", status_code=204)
+async def delete_classroom_endpoint(classroom_id: int):
+    """
+    Delete a classroom.
+    """
+    try:
+        await delete_classroom(classroom_id)
+        return Response(status_code=204)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ src/modules/classroom/services/upload_classrooms.py
@@ -0,0 +1,54 @@
+from src.database import database
+from typing import BinaryIO
+import pandas as pd
+import gc
+from fastapi import HTTPException
+from src.modules.classroom.models import ClassroomRequest
+from src.modules.classroom.services import create_classroom
+
+OWN_DEPARTMENT = "18"
+
+async def upload_classrooms_from_excel(file: BinaryIO) -> dict:
+    """
+    Upload classrooms from an Excel file.
+    """
+    try:
+        # Read Excel file
+        df = pd.read_excel(file, engine='openpyxl', sheet_name="Sistemas")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error reading Excel file: {str(e)}")
+
+    try:
+        for _, row in df.iterrows():
+            classrooms = str(row["AULA"]).strip() if pd.notnull(row["AULA"]) else None
+
+            if classrooms is None:
+                continue
+
+            # Separating classrooms
+            classrooms_list = [c.strip() for c in classrooms.split("|")]
+            for classroom in classrooms_list:
+                # Check if the classroom already exists in the database
+                existing_classroom = await database.classroom.find_first(
+                    where={"location": classroom}
+                )
+                if existing_classroom:
+                    continue
+
+                # Create a new ClassroomRequest object
+                classroom_request = ClassroomRequest(
+                    capacity=int(row["CUPO"]),
+                    location=classroom,
+                    ownDepartment=classroom.startswith(OWN_DEPARTMENT),
+                    virtualMode=classroom in ("INGENIA", "UDE@"),
+                )
+
+                # Create the classroom in the database
+                await create_classroom(classroom_request)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error creating classroom: {str(e)}")
+    finally:
+        del df
+        gc.collect()
+
+    return {"message": "Classrooms uploaded successfully"}
